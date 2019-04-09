db0391c05983b870cbabd7ff4d97dd6a89c48b77
Edward <almar1701@hotmail.com>
2019-04-09 18:07:01 +0800

    add numpy and pandas test

--- code/org/prediction/example/classandfunction.py
+++ code/org/prediction/example/classandfunction.py
@@ -26,3 +26,5 @@ class Octopus:
     def tell_me_about_the_octopus(self):
         print("This octopus is " + self.color + ".")
         print(self.name + " is the octopus's name.")
+class GoldenOctopus(Octopus):
+    pass   # without any customize methods
---
+++ code/org/prediction/example/numpytest.py
@@ -0,0 +1,18 @@
+# Import the numpy package as np
+import numpy as np
+
+
+weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
+# Create 2 numpy arrays from height and weight
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+print(type(np_height))
+
+# elements wise calculation
+bmi = np_weight / np_height ** 2
+print(bmi)
+print(bmi > 24)
+print(np_height * 2)
+print(bmi[bmi > 24]) # print the elements bigger than 24
